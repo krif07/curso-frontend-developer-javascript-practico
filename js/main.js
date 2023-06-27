e17ea20e8ff0119a43aaa219225172667c48da87
Cristian Fernando Dávila López <cristian.fernando.c.davila.lopez.-nd@disney.com>
2023-06-27 17:16:09 -0500

    Menus

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const navbarEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+navbarEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+
+function toggleDesktopMenu(){
+    desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
+}
