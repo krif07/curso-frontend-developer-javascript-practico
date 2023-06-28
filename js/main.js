fb81342dbb55e6e870174b72275da882a4486519
Cristian Fernando Dávila López <cristian.fernando.c.davila.lopez.-nd@disney.com>
2023-06-27 19:54:57 -0500

    Aside

--- js/main.js
+++ js/main.js
@@ -1,16 +1,28 @@
-const navbarEmail = document.querySelector('.navbar-email');
+const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
-navbarEmail.addEventListener('click', toggleDesktopMenu);
+const menuShoppingCart = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuShoppingCart.addEventListener('click', toggleShoppingCart);
 
 function toggleDesktopMenu(){
+    aside.classList.add('inactive');
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+    aside.classList.add('inactive');
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleShoppingCart(){
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+    aside.classList.toggle('inactive');
 }
